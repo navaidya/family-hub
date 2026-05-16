59e249a33c83630d4d46d6523b2cc5d937353515
Naval Vaidya <naval.x.vaidya@oracle.com>
2026-05-15 22:46:32 -0700

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 window.FAMILY_HUB_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBEhlAHPHN85P2D2-ia8CU11N7rvoZrYnA",
+  authDomain: "family-hub-3dc9a.firebaseapp.com",
+  projectId: "family-hub-3dc9a",
+  storageBucket: "family-hub-3dc9a.firebasestorage.app",
+  messagingSenderId: "65438264669",
+  appId: "1:65438264669:web:c960c491332172549d4cb4",
 };
 
 window.FAMILY_HUB_FIREBASE_OPTIONS = {
